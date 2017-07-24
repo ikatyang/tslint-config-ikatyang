c074667353b971aa2a99099002f606205a03f75e
Ika <ikatyang@gmail.com>
2017-07-24 15:04:05 +0800

    chore: enable prettier (#3)
    * chore: enable prettier
    
    * style: reformat with prettier

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,7 @@
 export {};
 // tslint:disable:no-unused-variable no-unused
 
-declare function do_something(): void;
+declare function do_something(): void
 declare const condition: boolean;
 declare const foo: boolean;
 declare const bar: boolean;
@@ -24,24 +24,44 @@ class MyClass {
   public my_public_property: any;
   protected _my_protected_property: any;
   private _my_private_property: any;
-  constructor() { /* ... */ }
-  public static my_static_public_method(): void { /* ... */ }
-  protected static _my_static_protected_method(): void { /* ... */ }
-  private static _my_static_private_method(): void { /* ... */ }
-  public my_public_method(): this { return this; }
-  protected _my_protected_method(): this { return this; }
-  private _my_private_method(): this { return this; }
+  constructor() {
+    /* ... */
+  }
+  public static my_static_public_method(): void {
+    /* ... */
+  }
+  protected static _my_static_protected_method(): void {
+    /* ... */
+  }
+  private static _my_static_private_method(): void {
+    /* ... */
+  }
+  public my_public_method(): this {
+    return this;
+  }
+  protected _my_protected_method(): this {
+    return this;
+  }
+  private _my_private_method(): this {
+    return this;
+  }
 }
 type MyType = any;
-interface MyInterface { my_property: any }
+interface MyInterface {
+  my_property: any;
+}
 const my_const = 0;
-function my_function(param_a: any, _param_b: any): void { /* ... */ }
+function my_function(param_a: any, _param_b: any): void {
+  /* ... */
+}
 
 // tslint:disable-next-line:no-use-before-declare https://github.com/palantir/tslint/issues/2551
-const {a: _a, ...b_and_c} = {a: 1, b: 2, c: 3};
+const { a: _a, ...b_and_c } = { a: 1, b: 2, c: 3 };
 
 // no-collapsible-if
-if (foo && bar) { do_something(); }
+if (foo && bar) {
+  do_something();
+}
 if (foo && bar) {
   do_something();
 }
